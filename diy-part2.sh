de48070e0c4993a25ca18a04c087d4f5525e62e8
ffff3333 <64050648+ffff3333@users.noreply.github.com>
2020-09-26 17:35:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.7.252/g' package/base-files/files/bin/config_generate
+git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
